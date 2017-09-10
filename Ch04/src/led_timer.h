3e216188b4c6693302b94a915c886dcad0222f2c
Alexander Jaworowski <alexander@jaworowski.se>
2017-09-10 16:30:22 +0200

    Some samll work with led_timer

--- Ch04/src/led_timer.h
+++ Ch04/src/led_timer.h
@@ -3,8 +3,8 @@
 
 void led_timer_set_frequency(float frq);
 void led_timer_start(void);
-//led_timer_stop(void);
-//led_timer_reset(void);
+void led_timer_stop(void);
+void led_timer_reset(void);
 //led_timer_init(void);
 
 #endif // _LED_TIMER_H
